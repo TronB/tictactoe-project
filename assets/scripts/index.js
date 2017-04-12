71d44a582cca45cbbb1aff5dd029363cb9cdf9cd
TronB <mraz5225@yahoo.com>
2017-04-12 11:36:09 -0400

    Added logic to not overwrite occupied cell

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,24 +8,27 @@ $(() => {
 })
 const gameBoard = []
 let turnCount = 1
-$(document).ready(function(){
+$(document).ready(function () {
 
   const onClickCell = function (evt) {
     // saves the position on the board that was clicked
-    const positionOnBoard = $(evt.target).data("id") // 1 or 2, etc
+    const positionOnBoard = $(evt.target).data('id') // 1 or 2, etc
 
     // assigns an x or o based on the turn
-    if (turnCount % 2 === 1){
-      gameBoard[positionOnBoard] = "x"
-    } else {
-      gameBoard[positionOnBoard] = "o"
+    if (turnCount % 2 === 1 && typeof gameBoard[positionOnBoard] !== 'string'){
+      gameBoard[positionOnBoard] = 'x'
+      turnCount++
+    }
+    else if (turnCount % 2 === 0 && typeof gameBoard[positionOnBoard] !== 'string') {
+      gameBoard[positionOnBoard] = 'o'
+      turnCount++
     }
 
     // add x or o to the position on the board
     $(evt.target).text(gameBoard[positionOnBoard])
 
     // increment turnCount
-    turnCount++
+    // turnCount++
 
     // if (typeof sqNum !== 'string') {
     //   const player1 = 'X'
@@ -40,7 +43,6 @@ $(document).ready(function(){
 
   console.log('what')
 })
-
 
 
 // use require with a reference to bundle the file and use it in this file
