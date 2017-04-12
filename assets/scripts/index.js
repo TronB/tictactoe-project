66f8872a2b183db5dfb47f72c22967d5a0e94760
TronB <mraz5225@yahoo.com>
2017-04-12 11:17:53 -0400

    Added turn logic and spits out "x" and "o"

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,6 +6,42 @@ const config = require('./config')
 $(() => {
   setAPIOrigin(location, config)
 })
+const gameBoard = []
+let turnCount = 1
+$(document).ready(function(){
+
+  const onClickCell = function (evt) {
+    // saves the position on the board that was clicked
+    const positionOnBoard = $(evt.target).data("id") // 1 or 2, etc
+
+    // assigns an x or o based on the turn
+    if (turnCount % 2 === 1){
+      gameBoard[positionOnBoard] = "x"
+    } else {
+      gameBoard[positionOnBoard] = "o"
+    }
+
+    // add x or o to the position on the board
+    $(evt.target).text(gameBoard[positionOnBoard])
+
+    // increment turnCount
+    turnCount++
+
+    // if (typeof sqNum !== 'string') {
+    //   const player1 = 'X'
+    //   return player1}
+    // else {
+    //   return false
+    // }
+    debugger;
+  }
+
+  $('.game-board td').on('click', onClickCell)
+
+  console.log('what')
+})
+
+
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
